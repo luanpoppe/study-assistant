34ef9ce04ab317d239cb0316b8574950a1129180
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-24 17:52:08 -0300

    feat: add frontend with tailwind and shadcn template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ModeToggle } from "./components/theme-toggle";
+
+function App() {
+  return (
+    <>
+      <ModeToggle />
+      <h1 className="text-red-500">Olá</h1>
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </>
+  );
+}
+
+export default App;
